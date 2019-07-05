bce407439942c71b46debb061d75a2c2c0402f88
Caro Berry <carolbpr@gmail.com>
2019-07-05 18:14:35 -0500

    positions for player, enemies and defender setup

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,55 +1,103 @@
 $(document).ready(function () {
     var playerselected = false;
-
-    if (playerselected === false) {
-        $("#bart").on("click", function () {
+    var defender = false;
+    var bart = null;
+    var maggie = null;
+    var lisa = null;
+    var ned = null;
+    $("#bart").on("click", function () {
+        if (playerselected === false) {
             //Selected Bart
             $("#boxBart").appendTo("#playerDiv");
             playerselected = true;
-            $("#boxBart").css("background-color", "#5edc4d");
+            bart = "player";
             $("#boxMaggie").appendTo("#enemieDiv");
             $("#boxMaggie").css("background-color", "red");
+            maggie = "enemie";
             $("#boxLisa").appendTo("#enemieDiv");
             $("#boxLisa").css("background-color", "red");
+            lisa = "enemie";
             $("#boxNed").appendTo("#enemieDiv");
             $("#boxNed").css("background-color", "red");
-        })
-        $("#maggie").on("click", function () {
+            ned = "enemie";
+        }
+        else if (($("#boxBart").css("background-color") === "rgb(255, 0, 0)") && (defender == false)) {
+            defender = true;
+            $("#boxBart").appendTo("#defenderDiv");
+            $("#boxBart").css("background-color", "black");
+        }
+    })
+    $("#maggie").on("click", function () {
+        if (playerselected === false) {
             //Selected Maggie
             $("#boxMaggie").appendTo("#playerDiv");
             playerselected = true;
-            $("#boxMaggie").css("background-color", "#5edc4d");
+            maggie = "player";
             $("#boxBart").appendTo("#enemieDiv");
             $("#boxBart").css("background-color", "red");
+            bart = "enemie"
             $("#boxLisa").appendTo("#enemieDiv");
             $("#boxLisa").css("background-color", "red");
+            lisa = "enemie";
             $("#boxNed").appendTo("#enemieDiv");
             $("#boxNed").css("background-color", "red");
-        });
-        $("#lisa").on("click", function () {
+            ned = "enemie";
+        }
+        else if (($("#boxMaggie").css("background-color") === "rgb(255, 0, 0)") && (defender == false)) {
+            defender = true;
+            $("#boxMaggie").appendTo("#defenderDiv");
+            $("#boxMaggie").css("background-color", "black");
+        }
+    })
+    $("#lisa").on("click", function () {
+        if (playerselected === false) {
             //Selected Lisa
             $("#boxLisa").appendTo("#playerDiv");
             playerselected = true;
-            $("#boxLisa").css("background-color", "#5edc4d");
+            lisa = "player";
             $("#boxBart").appendTo("#enemieDiv");
             $("#boxBart").css("background-color", "red");
+            bart = "enemie";
             $("#boxMaggie").appendTo("#enemieDiv");
             $("#boxMaggie").css("background-color", "red");
+            maggie = "enemie";
             $("#boxNed").appendTo("#enemieDiv");
             $("#boxNed").css("background-color", "red");
-        });
-        $("#ned").on("click", function () {
+            ned = "enemie";
+        }
+        else if (($("#boxLisa").css("background-color") === "rgb(255, 0, 0)") && (defender == false)){
+            defender = true;
+            $("#boxLisa").appendTo("#defenderDiv");
+            $("#boxLisa").css("background-color", "black");
+        }
+    })
+    $("#ned").on("click", function () {
+        if (playerselected === false) {
             //Selected Ned
             $("#boxNed").appendTo("#playerDiv");
             playerselected = true;
-            $("#boxNed").css("background-color", "#5edc4d");
+            ned = "player";
             $("#boxBart").appendTo("#enemieDiv");
             $("#boxBart").css("background-color", "red");
+            bart = "enemie";
             $("#boxMaggie").appendTo("#enemieDiv");
             $("#boxMaggie").css("background-color", "red")
+            maggie = "enemie";
             $("#boxLisa").appendTo("#enemieDiv");
             $("#boxLisa").css("background-color", "red");
-        });
-        
-    }
+            lisa = "enemie";
+        }
+        else if (($("#boxNed").css("background-color") === "rgb(255, 0, 0)") && (defender == false)){
+            defender = true;
+            $("#boxNed").appendTo("#defenderDiv");
+            $("#boxNed").css("background-color", "black");
+        }
+    })
+
+});
+$(document).ready(function () {
+
+    $("#bart").on("click", function () {
+
+    })
 });
